80271ddb73e2e404cabb7a184afff2c56d91cb5a
Tomasz <tplocic20@gmail.com>
2018-08-16 11:55:16 +0200

    Units editor

--- src/app/components/partials/units-editor/units-editor.component.ts
+++ src/app/components/partials/units-editor/units-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FireService} from "../../../services/fire.service";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-units-editor',
@@ -9,10 +10,34 @@ import {FireService} from "../../../services/fire.service";
 export class UnitsEditorComponent implements OnInit {
 
   units: any;
-  constructor(private srv: FireService) { }
+  constructor(private srv: FireService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.units = this.srv.units;
+  }
+
+  editUnit(unit, vm) {
+    vm.isEdit = true;
+    vm.form = this.fb.group(
+      {
+        name: [unit.Name, Validators.required]
+      }
+    );
+  }
+
+  saveUnit(unit, vm) {
+    if (vm.form.invalid) return;
+    unit.Name = vm.form.get('name').value;
+    this.srv.editUnit(unit).then(() => this.cancelEditUnit(vm));
+  }
+
+  removeUnit(unit) {
+    this.srv.removeUnit(unit);
+  }
+
+  cancelEditUnit(vm) {
+    delete vm.isEdit;
+    delete vm.form;
   }
 
 }
